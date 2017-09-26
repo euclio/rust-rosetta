bf74944e4ba7cfd5df160e89c4f7e00ca98f28b4
Andy Russell <arussell123@gmail.com>
2017-09-26 14:18:10 -0400

    fix bug in Sierpinski Triangle

--- tasks/sierpinski-triangle/src/main.rs
+++ tasks/sierpinski-triangle/src/main.rs
@@ -1,33 +1,22 @@
-#![feature(iterator_step_by)]
-
-use std::iter::repeat;
-
 fn main() {
-    let order = 4;
-    let height = 1 << order;
-    let mut state: Vec<bool> = repeat(true).take(height + 1).collect();
+    const ORDER: i32 = 4;
+    const HEIGHT: i32 = 1 << ORDER;
 
-    // Compute the triangle line-by-line by viewing it as Pascal's triangle (mod 2)
-    for i in 0..height {
-        for _ in 0..height - i - 1 {
+    for y in (0..HEIGHT).rev() {
+        for _ in 0..y {
             print!(" ");
         }
 
-        for filled in state.iter().take(i + 1) {
-            let fill = if *filled {
-                "*"
+        for x in 0..(HEIGHT - y) {
+            let fill = if x & y != 0 {
+                ' '
             } else {
-                " "
+                '*'
             };
 
-            print!(" {}", fill);
+            print!("{} ", fill);
         }
 
-        // Compute the next line
-        for j in (i as i32..0).rev().step_by(1) {
-            state[j as usize] ^= state[(j - 1) as usize];
-        }
-
-        print!("\n");
+        println!();
     }
 }
